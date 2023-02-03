8091837d8ee6a8f3825b446851bfdc0293eab9b7
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-02-03 11:18:37 -0600

    mejora de codigo JS

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ const btnChange = document.querySelector('button');
 
 btnChange.addEventListener('click', () => {
     if (btnChange.innerText === "Click me") {
+
         document.getElementById("aboutMe").scrollIntoView({behavior: "smooth"});
         btnChange.innerText = "Say hello";
+        
     }else if (btnChange.innerText === "Say hello") {
+
         document.getElementById("homeP").scrollIntoView({behavior: 'smooth'});
         btnChange.innerText = "Click me";
+        
     }
 });
 
